6c56005a70931406523e84a5122230664dfe994f
lmabaso <lmabaso@student.wethinkcode.co.za>
2018-09-21 09:49:28 +0200

    lmabaso

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmabaso <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/26 20:21:53 by lmabaso           #+#    #+#             */
+/*   Updated: 2018/07/01 17:54:07 by lmabaso          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
